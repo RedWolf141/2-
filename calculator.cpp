4eda263df93d67f52fc2888f4c6e828db4132cc1
RedWolf141 <41781661+RedWolf141@users.noreply.github.com>
2020-11-05 10:45:44 +0900

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -7,7 +7,7 @@ void getEqubation_1();
 void getEqubation_2();
 void getEqubation_3();
 
-void errorCheck(); //차수 입력문제, 근이 없음(허근), 잘못된 입력
+double eq_helper(double a, double b, double c);
 struct information
 {
 	int demension; // 방정식의 최고차항의 지수
@@ -15,23 +15,19 @@ struct information
 	int key_2;// 2차 방정식 해의 개수
 	int key_3; //3차 방정식 해의 개수
 	
-	int input_x1;
-	int input_x2;
-	int input_x3;
+	double input_x1;
+	double input_x2;
+	double input_x3;
 	char error[40];
 }equbation;
 /*
 error 모음집
-
  //SDE= set Dememsion Error 1차, 2차 3차 이외의 차수가 입력됬을때
  CHZ=Coefficient of highest order term is Zero 최고차항의 계수가 0
+ IR=Imaginary Root 
  */
 struct key
 {
-	int input_x1;
-	int input_x2;
-	int input_x3;
-
 	double x1;
 	double x2;
 	double x3;
@@ -41,20 +37,17 @@ struct key
 1차 ax+b=0
 2차 ax^2+bx+c=0
 3차 ax^3+bx^2+cx+d=0
-
 1차-> 
 x=-b/a (a!=0)
 2차->
 근의 공식 활용
 3차
-
 f(X)=(x-a)Q(x)형태로 조립제법
-
 */
 
 int main()
 {
-
+	printf("차수를 입력해주세요");
 	scanf("%d",&equbation.demension);
 
 	switch (equbation.demension) {
@@ -74,14 +67,16 @@ int main()
 
 	}
 
-	errorCheck();
+
+	
 }
 
 void getEqubation_1()
 {
-	scanf("%d %d", equbation.input_x1,equbation.input_x2);
-	int a = equbation.input_x1;
-	int b = equbation.input_x2;
+	printf("ax+b=0 의 형태 일차방정식을 입력해주세요.");
+	scanf("%d %d", &equbation.input_x1,&equbation.input_x2);
+	double a = equbation.input_x1;
+	double b = equbation.input_x2;
 
 	if (equbation.input_x1 == 0) {
 		strcpy(equbation.error,"CHZ");
@@ -89,15 +84,38 @@ void getEqubation_1()
 
 	else {
 		answer.x1=-b/a;
+		printf("방정식의 해는 = %lf", answer.x1);
 	}
-
 	
 }
 
 void getEqubation_2()
 
 {
+	
+	printf("ax^2+bx+c=0 의 형태 일차방정식을 입력해주세요.");
+	scanf("%d %d %d", &equbation.input_x1,&equbation.input_x2, &equbation.input_x3);
+	double a = equbation.input_x1;
+	double b = equbation.input_x2;
+	double c = equbation.input_x3;
+	
+	if (equbation.input_x1 == 0) {
+		printf("잘못된 입력값입니다");
+	}
 
+	else if (eq_helper(a,b,c)<0 ) // 2차방정식의 근의 개수가 0일경우 ( 허근) 
+	
+	{
+		printf("근이 없습니다"); 
+	}
+	
+	else 
+	{
+		answer.x1=(-b+sqrt(b*b-4*a*c))/2*a;
+		answer.x2=(-b-sqrt(b*b-4*a*c))/2*a;
+	}
+	
+	printf("%lf %lf", answer.x1, answer.x2);
 }
 
 
@@ -108,7 +126,7 @@ getEqubation_2();
 //추가 예정
 }
 
-void errorCheck()
+double eq_helper(double a, double b, double c)
 {
-
+	return b*b-4*a*c;
 }
