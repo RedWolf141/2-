5dca7257c4cb79ea592c5ba19722fbb3ca80c874
RedWolf141 <41781661+RedWolf141@users.noreply.github.com>
2020-11-19 10:45:59 +0900

    Add files via upload

---
+++ equbation_calculator.py
@@ -0,0 +1,16 @@
+import re
+import math
+
+def getEqubation_1():
+    a, b= float(input("ax+b=0 의 형태 일차방정식을 입력해주세요.")), float(input(""))
+
+demension=int(input("차수를 입력해주세요"))
+
+if (demsion==1):
+    pass
+elif (demsion==2):
+    pass
+elif (demsion==3):
+    pass
+else:
+    print("잘못된 입력값입니다")
