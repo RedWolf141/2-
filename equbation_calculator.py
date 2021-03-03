73c67328c70f4f6fd8e05f1f647a10d4ff65b9d3
RedWolf141 <41781661+RedWolf141@users.noreply.github.com>
2021-03-03 10:00:44 +0900

    Update equbation_calculator.py

--- equbation_calculator.py
+++ equbation_calculator.py
@@ -14,3 +14,32 @@ elif (demsion==3):
     pass
 else:
     print("잘못된 입력값입니다")
+/*
+#include <stdio.h>
+
+double eq_helper(double a, double b, double c)
+{
+	return b*b-4*a*c;
+}
+
+double linear_equation(double a, double b) {
+	return -1*b/a;
+}
+
+void cube_equbation(double a, double b, double c) 
+{
+	cube_x1 = (-1*b + sqrt(eq_helper(a,b,c)))/2*a
+	cube_x2 = (-1*b - sqrt(eq_helper(a,b,c)))/2*a
+}
+/*
+
+Linear equation:1차 방정식
+ax+b = 0
+x = -b/a
+
+cube equbation : 2차 방정식
+ax^2 + bx + c = 0
+
+x = -b/2a +/- sqrt(b^2+4ac)/2a
+
+*/
