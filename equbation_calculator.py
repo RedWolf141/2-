a19ecdc13af61ea168cb8dd4da7033991ccf8a52
RedWolf141 <41781661+RedWolf141@users.noreply.github.com>
2021-03-04 14:49:59 +0900

    Update equbation_calculator.py

--- equbation_calculator.py
+++ equbation_calculator.py
@@ -1,45 +1,21 @@
 import re
 import math
 
-def getEqubation_1():
-    a, b= float(input("ax+b=0 의 형태 일차방정식을 입력해주세요.")), float(input(""))
+dimension = int(input("방정식의 차원을 입력하세요"))
 
-demension=int(input("차수를 입력해주세요"))
+if (dimension == 1):
+    a, b = input('ax+b=0 의 형태를 입력하세요: ').split() # 공백을 기준으로 a,b를 입력
+    a = int(a)
+    b = int(b)
+    try:
+        answer = -b/a
+    except ZeroDivisionError:
+        print("일차항의 계수는 0이 아닌 실수여야 합니다")
+    else:
+        print("일차방정식의 해는", answer, "입니다")
+  
 
-if (demsion==1):
-    pass
-elif (demsion==2):
-    pass
-elif (demsion==3):
-    pass
-else:
-    print("잘못된 입력값입니다")
-/*
-#include <stdio.h>
 
-double eq_helper(double a, double b, double c)
-{
-	return b*b-4*a*c;
-}
 
-double linear_equation(double a, double b) {
-	return -1*b/a;
-}
 
-void cube_equbation(double a, double b, double c) 
-{
-	cube_x1 = (-1*b + sqrt(eq_helper(a,b,c)))/2*a
-	cube_x2 = (-1*b - sqrt(eq_helper(a,b,c)))/2*a
-}
-/*
 
-Linear equation:1차 방정식
-ax+b = 0
-x = -b/a
-
-cube equbation : 2차 방정식
-ax^2 + bx + c = 0
-
-x = -b/2a +/- sqrt(b^2+4ac)/2a
-
-*/
